29a5665a7a95d0242981ed91462fc832283afb78
Kaunain <kaunainreza27@gmail.com>
2022-09-08 10:53:45 +0530

    Initial commit

---
+++ app/src/main/java/com/example/apicall/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.apicall;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    String url = "https://jsonplaceholder.typicode.com/";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView =(TextView) findViewById(R.id.textView);
+        textView.setText("");
+
+        Retrofit retrofit = new Retrofit.Builder().baseUrl(url).addConverterFactory(GsonConverterFactory.create()).build();
+
+        MyAPI api = retrofit.create(MyAPI.class);
+
+        Call<List<Model>> call= api.getmodels();
+
+        call.enqueue(new Callback<List<Model>>() {
+            @Override
+            public void onResponse(Call<List<Model>> call, Response<List<Model>> response) {
+                List<Model> data = response.body();
+                for (int i=0;i<data.size();i++)textView.append("SL No:"+data.get(i).getId()+" \n Title :"
+                        +data.get(i).getTitle()+"\n\n\n");
+
+            }
+
+            @Override
+            public void onFailure(Call<List<Model>> call, Throwable t) {
+
+            }
+        });
+
+    }
+}
