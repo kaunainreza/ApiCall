51cf870fc71f001f9b152aa8c0b9b469406b8cfe
Kaunain <kaunainreza27@gmail.com>
2022-09-09 14:05:20 +0530

    Done

--- app/src/main/java/com/example/apicall/MainActivity.java
+++ app/src/main/java/com/example/apicall/MainActivity.java
@@ -1,12 +1,9 @@
 package com.example.apicall;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.widget.TextView;
-
 import java.util.List;
-
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
